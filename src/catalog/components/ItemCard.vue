5020fe7c12675dee76589402c56bf2a6d666705c
Doic <michalakradek2@gmail.com>
2022-11-25 19:46:26 +0100

    changed partly design of cards to elevated and recolored buttons

--- src/catalog/components/ItemCard.vue
+++ src/catalog/components/ItemCard.vue
@@ -3,7 +3,7 @@ defineProps(['title', 'type'])
 </script>
 
 <template>
-    <div class="max-w-sm border border-surface+1 rounded-lg shadow-md bg-surface+1 border-gray-700 hover:bg-surface-variant">
+    <div class="max-w-sm rounded-lg shadow-md bg-surface+1 hover:bg-surface-variant">
         <a href="#">
             <img class="rounded-t-lg max-w-full h-auto" src="../../assets/img/dummy-board-game.png" alt="Dummy board game"/>
         </a>
@@ -14,8 +14,8 @@ defineProps(['title', 'type'])
             </a>
             <p class="mb-3 font-normal text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             <div class="text-right">
-                <button type="button" class="text-white text-sm hover:bg-blue-800 focus:outline-none focus:ring-4 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">Edytuj</button>
-                <button type="button" class="text-sm text-red-700 hover:text-white border focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 border-red-500 text-red-500 hover:text-white hover:bg-red-600 focus:ring-red-900">Usuń</button>
+                <button type="button" class="text-black text-sm text-center font-medium rounded-full bg-primary px-5 py-2.5 mr-2 mb-2 hover:bg-primary-container hover:text-white focus:outline-none focus:ring-4 focus:ring-on-primary">Edytuj</button>
+                <button type="button" class="text-sm text-error text-center border border-error font-medium rounded-full px-5 py-2.5  mr-2 mb-2 hover:bg-error-container hover:border-error-container focus:ring-4 focus:outline-none focus:ring-on-error focus:bg-error-container focus:border-error-container">Usuń</button>
             </div>
         </div>
     </div>
--- src/catalog/pages/CatalogPage.vue
+++ src/catalog/pages/CatalogPage.vue
@@ -3,15 +3,15 @@ import { ref } from 'vue'
 import ItemCard from '../components/ItemCard.vue'
 
 const items = ref([
-  { id: 1, title: 'Przykładowa książka',        type: 'Książka'       },
-  { id: 2, title: 'Przykładowa gra planszowa',  type: 'Gra planszowa' },
-  { id: 3, title: 'Przykładowa gra planszowa',  type: 'Gra planszowa' },
-  { id: 4, title: 'Przykładowa gra planszowa',  type: 'Gra planszowa' },
-  { id: 5, title: 'Przykładowa książka',        type: 'Książka'       },
-  { id: 6, title: 'Przykładowa książka',        type: 'Książka'       },
-  { id: 7, title: 'Przykładowa gra planszowa',  type: 'Gra planszowa' },
-  { id: 8, title: 'Przykładowa książka',        type: 'Książka'       },
-  { id: 9, title: 'Przykładowa książka',        type: 'Książka'       },
+  { id: 1,  title: 'Przykładowa książka',       type: 'Książka'       },
+  { id: 2,  title: 'Przykładowa gra planszowa', type: 'Gra planszowa' },
+  { id: 3,  title: 'Przykładowa gra planszowa', type: 'Gra planszowa' },
+  { id: 4,  title: 'Przykładowa gra planszowa', type: 'Gra planszowa' },
+  { id: 5,  title: 'Przykładowa książka',       type: 'Książka'       },
+  { id: 6,  title: 'Przykładowa książka',       type: 'Książka'       },
+  { id: 7,  title: 'Przykładowa gra planszowa', type: 'Gra planszowa' },
+  { id: 8,  title: 'Przykładowa książka',       type: 'Książka'       },
+  { id: 9,  title: 'Przykładowa książka',       type: 'Książka'       },
   { id: 10, title: 'Przykładowa gra planszowa', type: 'Gra planszowa' },
   { id: 11, title: 'Przykładowa gra planszowa', type: 'Gra planszowa' },
   { id: 12, title: 'Przykładowa książka',       type: 'Książka'       }
@@ -21,7 +21,7 @@ const items = ref([
 <template>
   <div class="container bg-background py-8 mx-auto">
     <div class="text-center">
-      <button type="button" class="py-3 px-5 text-base font-medium text-center text-white bg-primary rounded-lg focus:ring-4 focus:outline-none bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">Dodaj nowy produkt</button>
+      <button type="button" class="py-3 px-5 text-base font-medium text-center text-black bg-primary rounded-lg hover:bg-primary-container hover:text-white focus:ring-4 focus:outline-none focus:ring-on-primary">Dodaj nowy produkt</button>
     </div>
     <div class="grid grid-cols-1 gap-6 pt-6 md:grid-cols-3 lg:grid-cols-4">
       <div class="flex justify-center" v-for="item in items">
