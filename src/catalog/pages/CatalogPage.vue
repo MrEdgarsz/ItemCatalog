9079bf587c624f8322f33091e685de6914638790
Daniel Obłąk <daniel.oblak99@gmail.com>
2022-11-29 20:35:43 +0100

    code formatting

--- src/catalog/pages/CatalogPage.vue
+++ src/catalog/pages/CatalogPage.vue
@@ -4,18 +4,18 @@ import RaisedButton from '@/common/components/buttons/RaisedButton.vue';
 import ItemCard from '../components/ItemCard.vue'
 
 const items = ref([
-  { id: 1,  name: 'Przykładowa książka',       type: 'Książka',        description: "" },
-  { id: 2,  name: 'Przykładowa gra planszowa', type: 'Gra planszowa',  description: "" },
-  { id: 3,  name: 'Przykładowa gra planszowa', type: 'Gra planszowa',  description: "" },
-  { id: 4,  name: 'Przykładowa gra planszowa', type: 'Gra planszowa',  description: "" },
-  { id: 5,  name: 'Przykładowa książka',       type: 'Książka',        description: "" },
-  { id: 6,  name: 'Przykładowa książka',       type: 'Książka',        description: "" },
-  { id: 7,  name: 'Przykładowa gra planszowa', type: 'Gra planszowa',  description: "" },
-  { id: 8,  name: 'Przykładowa książka',       type: 'Książka',        description: "" },
-  { id: 9,  name: 'Przykładowa książka',       type: 'Książka',        description: "" },
-  { id: 10, name: 'Przykładowa gra planszowa', type: 'Gra planszowa',  description: "" },
-  { id: 11, name: 'Przykładowa gra planszowa', type: 'Gra planszowa',  description: "" },
-  { id: 12, name: 'Przykładowa książka',       type: 'Książka',        description: "" }
+  { id: 1, name: 'Przykładowa książka', type: 'Książka', description: "" },
+  { id: 2, name: 'Przykładowa gra planszowa', type: 'Gra planszowa', description: "" },
+  { id: 3, name: 'Przykładowa gra planszowa', type: 'Gra planszowa', description: "" },
+  { id: 4, name: 'Przykładowa gra planszowa', type: 'Gra planszowa', description: "" },
+  { id: 5, name: 'Przykładowa książka', type: 'Książka', description: "" },
+  { id: 6, name: 'Przykładowa książka', type: 'Książka', description: "" },
+  { id: 7, name: 'Przykładowa gra planszowa', type: 'Gra planszowa', description: "" },
+  { id: 8, name: 'Przykładowa książka', type: 'Książka', description: "" },
+  { id: 9, name: 'Przykładowa książka', type: 'Książka', description: "" },
+  { id: 10, name: 'Przykładowa gra planszowa', type: 'Gra planszowa', description: "" },
+  { id: 11, name: 'Przykładowa gra planszowa', type: 'Gra planszowa', description: "" },
+  { id: 12, name: 'Przykładowa książka', type: 'Książka', description: "" }
 ])
 </script>
 
