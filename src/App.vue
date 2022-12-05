25e4721ade0c4db5769ea7b2375306e346e6d3dd
Daniel Obłąk <daniel.oblak99@gmail.com>
2022-12-05 20:54:44 +0100

    Fixed issue with white space under the pages

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import NavigationRail from './common/components/NavigationRail.vue';
 </script>
 
 <template>
-  <div class=" text-neutral-50 flex flex-row bg-background ">
+  <div class=" min-h-screen text-neutral-50 flex flex-row bg-background ">
     <NavigationRail class="" />
     <div class="mx-auto desktop:px-10 laptop:px-5 tablet:px-2 phone-landscape:px-5 desktop:w-desktop ">
       <RouterView class=" " />
