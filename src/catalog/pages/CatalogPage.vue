fdb4cf312eb16c569c500ae8e7dae352d92fd1c8
Doic <michalakradek2@gmail.com>
2023-01-22 20:35:44 +0100

    added simple filter in CatalogPage

--- src/catalog/pages/CatalogPage.vue
+++ src/catalog/pages/CatalogPage.vue
@@ -5,6 +5,8 @@ import ItemCard from '../components/ItemCard.vue'
 import router from '@/router';
 import TextButton from '@/common/components/buttons/TextButton.vue';
 import IconButton from '@/common/components/buttons/IconButton.vue';
+import SelectInput from '@/common/components/inputs/SelectInput.vue';
+import TextInput from '@/common/components/inputs/TextInput.vue';
 
 const items = ref([
   { id: 1, name: 'Przykładowa książka', type: 'Książka', description: "" },
@@ -35,13 +37,28 @@ function navigateToEditPage() {
   <div class="container bg-background py-8 mx-auto">
     <div class="text-center">
       <RaisedButton label="Dodaj nowy produkt" @click="navigateToAddPage" />
+      <br/>
+      <div class="flex flex-col">
+        <div class="mx-auto">
+          <div class="bg-surface+1 p-6 rounded-lg shadow-md">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                <TextInput label="Nazwa produktu" />
+                <SelectInput :options="['Książka', 'Gra Planszowa']"
+                              label="Kategoria produktu" />
+                <SelectInput :options="['Nazwa: Alfabetycznie', 'Data: Od najnowszych', 'Data: Od najstarszych']"
+                        label="Sortowanie" />
+            </div>
+            <TextButton class="mr-2" label="Resetuj" variant="error" />
+            <RaisedButton label="Szukaj" />
+          </div>
+        </div>
+      </div>
     </div>
     <div class="grid grid-cols-4 gap-6 pt-6 phone-landscape:grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-3">
       <div class="flex justify-center" v-for="item in items">
         <ItemCard class="flex flex-col" :key="item.id" :name="item.name" :type="item.type"
           :description="item.description">
           <div class="grid grid-cols-2 grid-rows-1">
-
             <div class="flex items-center justify-start">
               <IconButton class="mr-2 " icon="favorite" id="favorite-button" />
             </div>
