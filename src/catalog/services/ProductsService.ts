f82cb3c9f876e8c34e126e13d4d48854069b0e61
Doic <michalakradek2@gmail.com>
2023-01-24 21:17:56 +0100

    added getWithFilters method in ProductsService

--- src/catalog/services/ProductsService.ts
+++ src/catalog/services/ProductsService.ts
@@ -7,6 +7,7 @@ import { ProductFactory } from "../factories/ProductFactory";
 import type { ProductsInterface } from "../interfaces/ProductInterface";
 import type { ProductDto } from "../models/dtos/ProductDto";
 import type { ProductInputDto } from "../models/dtos/ProductInputDto";
+import type { ProductFilterDto } from "../models/dtos/ProductFilterDto";
 import type { Product } from "../models/Product";
 
 export class ProductsService implements ProductsInterface {
@@ -23,13 +24,35 @@ export class ProductsService implements ProductsInterface {
             return left(new ServerError());
         }
     }
+    async getWithFilters(productFilterDto: ProductFilterDto): Promise<Either<AppException, Product[]>> {
+        const { name, category, sort, order } = productFilterDto;
+        const params = {};
+
+        if (name)       { params.name = name }
+        if (category)   { params.category = category }
+        if (sort)       { params.sort = sort }
+        if (order)      { params.order = order }
+
+        const response = await AxiosClient.instance.get<Array<ProductDto>>('products', {
+            params: params
+        });
+
+        if (response.status === 200) {
+            const filteredProducts: Product[] = [];
+            response.data.map((value) => {
+                filteredProducts.push(ProductFactory.fromDto(value))
+            });
+            return right(filteredProducts);
+        } else {
+            return left(new ServerError());
+        }
+    }
     async addProduct(productInputDto: ProductInputDto): Promise<Either<AppException, Product>> {
         const formData = ProductDtoFactory.toFormData(productInputDto);
         const response = await AxiosClient.instance.post<ProductDto>("products", formData, { headers: { 'Content-Type': 'multipart/form-data' }, });
         if (response.status === 200) {
             const product: Product = ProductFactory.fromDto(response.data);
             return right(product);
-
         } else if (response.status === 401) {
             return left(new UnauthorizedException());
         } else {
