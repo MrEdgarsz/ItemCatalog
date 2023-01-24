aa7ffe02e19626752773cb557fcbf90a9346c427
Daniel Obłąk <daniel.oblak99@gmail.com>
2023-01-24 21:09:03 +0100

    Code formatting in AuthStore, removed unnecessary logs

--- src/auth/stores/AuthStore.ts
+++ src/auth/stores/AuthStore.ts
@@ -2,27 +2,24 @@ import { defineStore } from "pinia";
 import type { AuthTokenDto } from "../models/dtos/AuthTokenDto";
 
 export const useAuthStore = defineStore('auth', {
-    state : () => ({
+    state: () => ({
         token: null as string | null,
         expiry: null as Date | null
     }),
-    getters : {
-        isExpired(): boolean{
+    getters: {
+        isExpired(): boolean {
             return this.expiry == null || this.expiry <= new Date();
         },
         isAuthenticated(): boolean {
             return this.token != null && !this.isExpired;
         },
     },
-    actions : {
-        setToken(dto: AuthTokenDto){
+    actions: {
+        setToken(dto: AuthTokenDto) {
 
             this.token = dto.access_token;
             this.expiry = new Date(dto.expires_at);
-            console.log('expiry: '+ this.expiry);
-            console.log('token: '+ this.token);
-            console.log('isExpired: '+ this.isExpired);
-            console.log('isAuthenticated: '+ this.isAuthenticated);
+
         }
     }
 })
